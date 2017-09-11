d40e3224061ede97d376bc9b14ffebc734027a53
Zach Schuermann <zachary.zvs@gmail.com>
2017-09-11 12:03:55 -0500

    TrialTest EOL past 120 fix

--- Project1/project1/TrialTest.java
+++ Project1/project1/TrialTest.java
@@ -30,8 +30,10 @@ public class TrialTest
         // Average, max, min etc. calculated and given in testData_info.rtf in testData package
         Assert.assertEquals("getMaxLeftWrist for valid data", .109172, testTrial.getMaxLeftWrist(0), .000001);
         Assert.assertEquals("getMinLeftWrist for valid data", .104375, testTrial.getMinLeftWrist(0), .000001);
-        Assert.assertEquals("getAverageLeftWrist for valid data", .106955438, testTrial.getAverageLeftWrist(0), .000000001);
-        Assert.assertEquals("getState for valid data", new State("0,0.105938,0.156901,-0.214572,0.157862,-0.273852,-0.055263").toString(), 
+        Assert.assertEquals("getAverageLeftWrist for valid data", .106955438, 
+                testTrial.getAverageLeftWrist(0), .000000001);
+        Assert.assertEquals("getState for valid data", 
+                new State("0,0.105938,0.156901,-0.214572,0.157862,-0.273852,-0.055263").toString(), 
                 testTrial.getState(0).toString());
     }
     
@@ -55,8 +57,10 @@ public class TrialTest
         // Average, max, min etc. calculated and given in testData_info.rtf in testData package
         Assert.assertEquals("getMaxLeftWrist for NaN data", .162172, testTrial.getMaxLeftWrist(0), .000001);
         Assert.assertEquals("getMinLeftWrist for NaN data", .315919, testTrial.getMinLeftWrist(1), .000001);
-        Assert.assertEquals("getAverageLeftWrist for NaN data", -.033168091, testTrial.getAverageLeftWrist(2), .000000001);
-        Assert.assertEquals("getState for NaN data", new State("0.22,NaN,NaN,NaN,0.117181,-0.310722,-0.066776").toString(), 
+        Assert.assertEquals("getAverageLeftWrist for NaN data",
+                -.033168091, testTrial.getAverageLeftWrist(2), .000000001);
+        Assert.assertEquals("getState for NaN data",
+                new State("0.22,NaN,NaN,NaN,0.117181,-0.310722,-0.066776").toString(), 
                 testTrial.getState(11).toString());
         
     }
