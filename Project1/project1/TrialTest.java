7e9db968e85b988189d9e7268ac26f28270b9bcd
Zach Schuermann <zachary.zvs@gmail.com>
2017-09-11 12:01:17 -0500

    Trialtest test messages added
    And misc documentation

--- Project1/project1/TrialTest.java
+++ Project1/project1/TrialTest.java
@@ -21,17 +21,17 @@ public class TrialTest
         Trial testTrial = new Trial("testData", "testValid", 0);
         
         // Test getters
-        Assert.assertEquals("testValid", testTrial.getInfantID());
-        Assert.assertEquals("testData/subject_testValid_w00.csv", testTrial.getFileName());
-        Assert.assertEquals(48, testTrial.getSize());
-        Assert.assertEquals(00, testTrial.getWeek());
+        Assert.assertEquals("InfantID test", "testValid", testTrial.getInfantID());
+        Assert.assertEquals("Filename from testData", "testData/subject_testValid_w00.csv", testTrial.getFileName());
+        Assert.assertEquals("Getsize for trial", 48, testTrial.getSize());
+        Assert.assertEquals("Getweek for trial", 00, testTrial.getWeek());
         
         // Test calculation methods and toString
         // Average, max, min etc. calculated and given in testData_info.rtf in testData package
-        Assert.assertEquals(.109172, testTrial.getMaxLeftWrist(0), .000001);
-        Assert.assertEquals(.104375, testTrial.getMinLeftWrist(0), .000001);
-        Assert.assertEquals(.106955438, testTrial.getAverageLeftWrist(0), .000000001);
-        Assert.assertEquals(new State("0,0.105938,0.156901,-0.214572,0.157862,-0.273852,-0.055263").toString(), 
+        Assert.assertEquals("getMaxLeftWrist for valid data", .109172, testTrial.getMaxLeftWrist(0), .000001);
+        Assert.assertEquals("getMinLeftWrist for valid data", .104375, testTrial.getMinLeftWrist(0), .000001);
+        Assert.assertEquals("getAverageLeftWrist for valid data", .106955438, testTrial.getAverageLeftWrist(0), .000000001);
+        Assert.assertEquals("getState for valid data", new State("0,0.105938,0.156901,-0.214572,0.157862,-0.273852,-0.055263").toString(), 
                 testTrial.getState(0).toString());
     }
     
@@ -46,17 +46,17 @@ public class TrialTest
         Trial testTrial = new Trial("testData", "testNaN", 0);
         
         // Test getters
-        Assert.assertEquals("testNaN", testTrial.getInfantID());
-        Assert.assertEquals("testData/subject_testNaN_w00.csv", testTrial.getFileName());
-        Assert.assertEquals(24, testTrial.getSize());
-        Assert.assertEquals(00, testTrial.getWeek());
+        Assert.assertEquals("InfantID test", "testNaN", testTrial.getInfantID());
+        Assert.assertEquals("Filename from testData", "testData/subject_testNaN_w00.csv", testTrial.getFileName());
+        Assert.assertEquals("Getsize for trial", 24, testTrial.getSize());
+        Assert.assertEquals("Getweek for trial", 00, testTrial.getWeek());
         
         // Test calculation methods and toString
         // Average, max, min etc. calculated and given in testData_info.rtf in testData package
-        Assert.assertEquals(.162172, testTrial.getMaxLeftWrist(0), .000001);
-        Assert.assertEquals(.315919, testTrial.getMinLeftWrist(1), .000001);
-        Assert.assertEquals(-.033168091, testTrial.getAverageLeftWrist(2), .000000001);
-        Assert.assertEquals(new State("0.22,NaN,NaN,NaN,0.117181,-0.310722,-0.066776").toString(), 
+        Assert.assertEquals("getMaxLeftWrist for NaN data", .162172, testTrial.getMaxLeftWrist(0), .000001);
+        Assert.assertEquals("getMinLeftWrist for NaN data", .315919, testTrial.getMinLeftWrist(1), .000001);
+        Assert.assertEquals("getAverageLeftWrist for NaN data", -.033168091, testTrial.getAverageLeftWrist(2), .000000001);
+        Assert.assertEquals("getState for NaN data", new State("0.22,NaN,NaN,NaN,0.117181,-0.310722,-0.066776").toString(), 
                 testTrial.getState(11).toString());
         
     }
