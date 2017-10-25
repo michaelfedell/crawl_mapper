020814bf3ec0656f50dc4073b9712ffd1549e776
Zach Schuermann <zachary.zvs@gmail.com>
2017-10-25 13:36:57 -0500

    Fix NPE for FieldMapper

--- ProjectMaster/project/FieldMapperTest.java
+++ ProjectMaster/project/FieldMapperTest.java
@@ -20,6 +20,12 @@ public class FieldMapperTest
                 + "left_foot_y,left_foot_z,right_foot_x,right_foot_y,right_foot_z,upper_back_x,upper_back_y,"
                 + "upper_back_z,sippc_action,robot_vel_l,robot_vel_r";
     
+    private String secondRow = "0,0.164617,0.293503,-0.092803,0.314738,-0.113438,-0.154972,0.140147,0.230248,"
+            + "-0.016396,0.259088,-0.16724,-0.088291,0.211352,0.115696,-0.025542,0.243218,-0.081355,0.014899,"
+            + "-0.120688,0.040215,-0.025421,-0.113292,-0.10138,-0.045445,-0.249756,-0.004755,0.021516,-0.244225,"
+            + "-0.148719,-0.084166,-0.260507,-0.045713,-0.071859,-0.246074,-0.161261,-0.185907,0.227285,0.01717,"
+            + "-0.005322,0,-0.010565,-0.026693";
+    
     /**
      * Class to test the size of a fieldMapper
      */
@@ -30,6 +36,19 @@ public class FieldMapperTest
         FieldMapper fm = new FieldMapper(firstRow.split(","));
         Assert.assertEquals("Size test for fieldMapper", 16, fm.size());
     }
+    
+    /**
+     * Class to test contructing a pointND from null
+     */
+    @Test 
+    public void nullPointTest()
+    {
+        // instantiate fieldmapper using firstRow above and test size
+        FieldMapper fm = new FieldMapper(firstRow.split(","));
+        PointND testPoint = fm.extractPointND(secondRow.split(","), "bogus");
+        Assert.assertNull(testPoint);
+    }
+    
     
     /**
      * Class to test the field getter
